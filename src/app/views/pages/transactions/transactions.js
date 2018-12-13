a639703bebcf7476dd2f3b049274d1d3316e5c41
Corentin Leman <corentin.leman@gmail.com>
2018-12-12 23:32:17 -0600

    - Grouped Transactions by exchange. - Added edit and delete buttons.

--- src/app/views/pages/transactions/transactions.js
+++ src/app/views/pages/transactions/transactions.js
@@ -1,5 +1,6 @@
-import { Button, Container, Table, Label, Menu, Icon } from 'semantic-ui-react';
+import { Button, Container, Table, Header, Menu, Icon, Segment, Grid } from 'semantic-ui-react';
 import { connect } from "react-redux";
+import groupBy from 'lodash/groupBy';
 import { Link, Redirect } from 'react-router-dom';
 import PropTypes from "prop-types";
 import React, { Component } from 'react';
@@ -11,6 +12,14 @@ import { transactionsThunks } from 'state/ducks/transactions';
 import { withResponsiveWrapper } from 'enhancers';
 
 export class Transactions extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      orderedTransactions: {},
+    };
+  }
+
   componentWillMount() {
     const {
       getCurrencies,
@@ -27,6 +36,13 @@ export class Transactions extends Component {
     }
   };
 
+  componentDidUpdate(prevProps) {
+    const { transactions } = this.props;
+    if (transactions !== prevProps.transactions) {
+      this.setState({ orderedTransactions: groupBy(transactions, 'source') })
+    }
+  }
+
   addTransactionWrapper = transaction => {
     const { addTransaction, user } = this.props;
 
@@ -42,107 +58,112 @@ export class Transactions extends Component {
     });
   };
 
+  onEdit = transaction => {
+    console.log(transaction);
+  };
+
+  onDelete = transaction => {
+    console.log(transaction);
+  }
+
   render() {
-    const { currencies, transactions, addTransactionStatus } = this.props;
+    const { currencies, addTransactionStatus } = this.props;
+
+    const { orderedTransactions } = this.state;
 
     return (
-      <Container className={styles.exchange}>
-        <Table celled color='red' selectable sortable>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Source</Table.HeaderCell>
-              <Table.HeaderCell>Date</Table.HeaderCell>
-              <Table.HeaderCell>Type</Table.HeaderCell>
-              <Table.HeaderCell>Price</Table.HeaderCell>
-              <Table.HeaderCell>Quantity</Table.HeaderCell>
-              <Table.HeaderCell>Currency 1</Table.HeaderCell>
-              <Table.HeaderCell>Currency 2</Table.HeaderCell>
-              <Table.HeaderCell />
-            </Table.Row>
-          </Table.Header>
+      <div>
+        <Container className={styles.buttonsWrapper}>
+          <Segment color="grey">
+            <AddTransactionModal
+              addTransaction={this.addTransactionWrapper}
+              addTransactionStatus={addTransactionStatus}
+              sourceCurrencies={currencies}
+            />
+            <Button
+              as='label'
+              floated='right'
+              htmlFor='upload'
+              icon
+              labelPosition='left'
+              primary size='large'
+            >
+              <Icon name='upload' /> Import Transactions
+              <input
+                  hidden
+                  id='upload'
+                  multiple
+                  type="file"
+                  onChange={this.onChangeFile} />
+            </Button>
+          </Segment>
+        </Container>
 
-          <Table.Body>
-            {transactions.map(transaction => {
-              return (
-                <Table.Row key={transaction.id}>
-                  <Table.Cell>{transaction.source}</Table.Cell>
-                  <Table.Cell>{transaction.date}</Table.Cell>
-                  <Table.Cell>{transaction.type}</Table.Cell>
-                  <Table.Cell>{transaction.price}</Table.Cell>
-                  <Table.Cell>{transaction.quantity}</Table.Cell>
-                  <Table.Cell>{transaction.currency1}</Table.Cell>
-                  <Table.Cell>{transaction.currency2}</Table.Cell>
-                  <Table.Cell collapsing>
-                    <Button
-                      animated='vertical'
-                      floated='right'
-                      htmlFor='edit'
-                      size='mini'
-                    >
-                      <Button.Content hidden>Edit</Button.Content>
-                      <Button.Content visible>
-                        <Icon name='edit' />
-                      </Button.Content>
-                    </Button>
-                    <Button
-                      animated='vertical'
-                      color='red'
-                      floated='right'
-                      htmlFor='edit'
-                      size='mini'
-                    >
-                      <Button.Content hidden>Delete</Button.Content>
-                      <Button.Content visible>
-                        <Icon name='delete' />
-                      </Button.Content>
-                    </Button>
-                  </Table.Cell>
-                </Table.Row>
-              )
-            })}
-          </Table.Body>
+        <Grid divided='vertically'>
+          {Object.keys(orderedTransactions).map(exchange => (
+            <Grid.Row columns={1}>
+              <Grid.Column>
+                <Container className={styles.exchange}>
+                  <Header as='h1'>{exchange.toUpperCase()}</Header>
+                  <Table celled color='red' sortable striped>
+                    <Table.Header>
+                      <Table.Row>
+                        <Table.HeaderCell>Date</Table.HeaderCell>
+                        <Table.HeaderCell>Type</Table.HeaderCell>
+                        <Table.HeaderCell>Price</Table.HeaderCell>
+                        <Table.HeaderCell>Quantity</Table.HeaderCell>
+                        <Table.HeaderCell>Currency 1</Table.HeaderCell>
+                        <Table.HeaderCell>Currency 2</Table.HeaderCell>
+                        <Table.HeaderCell />
+                      </Table.Row>
+                    </Table.Header>
 
-          <Table.Footer>
-            <Table.Row>
-              <Table.HeaderCell colSpan='8'>
-                <Menu floated='right' pagination>
-                  <Menu.Item as='a' icon>
-                    <Icon name='chevron left' />
-                  </Menu.Item>
-                  <Menu.Item as='a'>1</Menu.Item>
-                  <Menu.Item as='a'>2</Menu.Item>
-                  <Menu.Item as='a'>3</Menu.Item>
-                  <Menu.Item as='a'>4</Menu.Item>
-                  <Menu.Item as='a' icon>
-                    <Icon name='chevron right' />
-                  </Menu.Item>
-                </Menu>
-                <Button
-                  floated='right'
-                  htmlFor='upload'
-                  icon
-                  labelPosition='left'
-                  primary size='large'
-                >
-                  <Icon name='upload' /> Import Transactions
-                  <input
-                      hidden
-                      id='upload'
-                      multiple
-                      type="file"
-                      onChange={this.onChangeFile} />
-                </Button>
-                <AddTransactionModal
-                  addTransaction={this.addTransactionWrapper}
-                  addTransactionStatus={addTransactionStatus}
-                  floated='right'
-                  sourceCurrencies={currencies}
-                />
-              </Table.HeaderCell>
-            </Table.Row>
-          </Table.Footer>
-        </Table>
-      </Container>
+                    <Table.Body>
+                      {orderedTransactions[exchange].map(transaction => (
+                        <Table.Row key={transaction.id}>
+                          <Table.Cell>{transaction.date}</Table.Cell>
+                          <Table.Cell>{transaction.type}</Table.Cell>
+                          <Table.Cell>{transaction.price}</Table.Cell>
+                          <Table.Cell>{transaction.quantity}</Table.Cell>
+                          <Table.Cell>{transaction.currency1}</Table.Cell>
+                          <Table.Cell>{transaction.currency2}</Table.Cell>
+                          <Table.Cell collapsing>
+                            <Button.Group size='tiny'>
+                              <Button
+                                animated='vertical'
+                                htmlFor='edit'
+                                onClick={() => this.onEdit(transaction)}
+                                size='mini'
+                              >
+                                <Button.Content hidden>Edit</Button.Content>
+                                <Button.Content visible>
+                                  <Icon name='edit' />
+                                </Button.Content>
+                              </Button>
+                              <Button
+                                animated='vertical'
+                                color='red'
+                                htmlFor='edit'
+                                onClick={() => this.onDelete(transaction)}
+                                size='mini'
+                              >
+                                <Button.Content hidden>Delete</Button.Content>
+                                <Button.Content visible>
+                                  <Icon name='delete' />
+                                </Button.Content>
+                              </Button>
+                            </Button.Group>
+                          </Table.Cell>
+                        </Table.Row>
+                      ))}
+                    </Table.Body>
+                  </Table>
+                </Container>
+              </Grid.Column>
+            </Grid.Row>
+          ))}
+        </Grid>
+      </div>
     )
   };
 };
